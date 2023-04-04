1b2850e64acc0e7388dbd57ade40beec6fb7cf17
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-05 00:20:19 +0500

    Реализовал хендлер освобождение товара на складе

--- internal/handlers/release.go
+++ internal/handlers/release.go
@@ -1,7 +1,60 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"go-test-lamoda/internal/database"
+	"net/http"
+)
+
+type ReleaseRequest struct {
+	UniqCodes []int `json:"uniq_codes"`
+}
 
 func ReleaseHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Неверный метод, необходим метод POST", http.StatusMethodNotAllowed)
+		return
+	}
 
+	// получаем список уникальных кодов товаров для освобождения
+	var codes ReleaseRequest
+	err := json.NewDecoder(r.Body).Decode(&codes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// начинаем транзакцию
+	tx, err := database.DB.Begin()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer tx.Rollback() // откатываем транзакцию в случае ошибки
+
+	// блокируем таблицу "products", чтобы другие клиенты не могли изменять данные
+	_, err = tx.Exec("LOCK TABLE products IN SHARE MODE")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// обновляем значения поля "feature" в таблице "stocks" на "available" для указанных товаров
+	for _, code := range codes.UniqCodes {
+		_, err = tx.Exec("UPDATE stocks SET feature='available' WHERE feature='reserved' AND id=(SELECT stocks_id FROM stocks_products WHERE products_id=(SELECT id FROM products WHERE uniq_code=$1))", code)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// коммитим транзакцию
+	err = tx.Commit()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
