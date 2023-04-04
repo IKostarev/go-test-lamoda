631d98dfdb3aa8a702493ae0e3acd781adcf512c
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-05 00:17:57 +0500

    Add default values in sql

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -2,16 +2,16 @@ CREATE TABLE stocks
 (
     id serial not null unique,
     name varchar(255) not null,
-    feature varchar(255) not null
+    feature varchar(255) not null default 'available'
 );
 
 CREATE TABLE products
 (
     id serial not null unique,
     name varchar(255) not null,
-    size integer not null,
-    uniq_code integer not null,
-    count integer not null
+    size integer not null default 0,
+    uniq_code integer not null default 0,
+    count integer not null default 0
 );
 
 CREATE TABLE stocks_products
