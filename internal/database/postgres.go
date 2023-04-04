0f4444b763422a7a1dd0640ff42396113806b830
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-05 00:20:46 +0500

    upd postgres connect

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -15,9 +15,12 @@ type Config struct {
 	SSLMode  string
 }
 
+var DB *sqlx.DB
+
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
-	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s username=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode))
+	db, err := sqlx.Open("postgres",
+		fmt.Sprintf("host=%s port=%s username=%s password=%s dbname=%s sslmode=%s",
+			cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode))
 	if err != nil {
 		return nil, err
 	}
